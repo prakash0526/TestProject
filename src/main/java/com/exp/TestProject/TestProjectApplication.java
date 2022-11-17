e2571b979fd40f33a31f5590bc8ae5d8c2a10797
unknown <prakash0526>
2022-11-17 12:55:30 +0530

    first commit

---
+++ src/main/java/com/exp/TestProject/TestProjectApplication.java
@@ -0,0 +1,41 @@
+package com.exp.TestProject;
+
+import com.exp.TestProject.model.Product;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class TestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectApplication.class, args);
+
+		TestProjectApplication ts = new TestProjectApplication();
+		TestProjectApplication.getProductsList();
+	}
+
+	private static void getProductsList() {
+		Product product1 = new Product();
+		Product product2 = new Product();
+
+		product1.setCategory("Books");
+		product1.setPrice(102);
+
+		product2.setCategory("pens");
+		product2.setPrice(30);
+
+		List<Product> productList = Arrays.asList(product1,product2).stream()
+				.filter(p -> p.getCategory().equalsIgnoreCase("Books"))
+				.filter(p -> p.getPrice() > 100)
+				.collect(Collectors.toList());
+
+		System.out.println(productList);
+		System.out.println("Hello its done");
+
+	}
+
+}
---
+++ src/main/java/com/exp/TestProject/model/Product.java
@@ -0,0 +1,9 @@
+package com.exp.TestProject.model;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private String category;
+    private int price;
+}
